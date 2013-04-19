4d8bdea12244657935ccd999dc40f3d4e499c49c
Servicios de mercadeo <ingenieria@movilmultimediasa.com>
2013-04-19 14:42:00 -0600

    Done
    done

--- myScripts.js
+++ myScripts.js
@@ -1,10 +1,14 @@
 
 function doIt()
 {
-$.post('https://movilmultimediasa.com/abcMobil/post.php', function(data) {
-  alert(data);
-});
-alert("4");
+$.ajax({
+				type: "POST",
+				url:  "compartir.php",  
+				data: {etiquetados: etiquetados},
+				success: function(data) {				
+				alert("La receta se compartió con éxito puedes ver la publicación en tu muro");				
+				}
+			});
 	
 }
 
