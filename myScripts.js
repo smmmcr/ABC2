bc5a7c821f3fc41ff910a710f9ada291f4c9da5e
Servicios de mercadeo <ingenieria@movilmultimediasa.com>
2013-04-19 13:47:38 -0600

    jsonp
    jsonp

--- myScripts.js
+++ myScripts.js
@@ -1,8 +1,40 @@
 
 function doIt()
 {
-	alert("is done");
-	return "doIt() is done";
+var url = 'http://www.jquery4u.com/scripts/jquery4u-sites.json?callback=?';
+/*
+$.ajax({
+   type: 'GET',
+    url: url,
+    async: false,
+    jsonpCallback: 'jsonCallback',
+    contentType: "application/json",
+    dataType: 'jsonp',
+    success: function(json) {
+       console.dir(json.sites);
+    },
+    error: function(e) {
+       console.log(e.message);
+    }
+});*/
+userName="dd";
+password="dd";
+ $.ajax
+    ({
+        type: "POST",
+        //the url where you want to sent the userName and password to
+        url: 'https://movilmultimediasa.com/abcMobil/post.php',
+        dataType: 'jsonp',
+        async: false,
+        //json object to sent to the authentication url
+        data: '{"userName": "' + userName + '", "password" : "' + password + '"}',
+        success: function () {
+
+        alert("Thanks!"); 
+        }
+    })
+
+	
 }
 
 function sum(a,b)
