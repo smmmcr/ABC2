193d2d2bddfc064e896ef54bd8de17bc65ca6778
Servicios de mercadeo <ingenieria@movilmultimediasa.com>
2013-04-19 14:28:36 -0600

    Revert "d"
    This reverts commit 1f19b40b7665c8b97e0a50b585203cd79a1e74cb.

--- myScripts.js
+++ myScripts.js
@@ -1,9 +1,38 @@
 
 function doIt()
 {
-$.post('https://movilmultimediasa.com/abcMobil/post.php', function(data) {
-  alert(data);
-});
+var url = 'http://www.jquery4u.com/scripts/jquery4u-sites.json?callback=?';
+/*
+$.ajax({
+   type: 'GET',
+    url: url,
+    async: false,
+    jsonpCallback: 'jsonCallback',
+    contentType: "application/json",
+    dataType: 'jsonp',
+    success: function(json) {
+       console.dir(json.sites);
+    },
+    error: function(e) {
+       console.log(e.message);
+    }
+});*/
+userName="dd";
+password="dd";
+ $.ajax
+    ({
+        type: "POST",
+        //the url where you want to sent the userName and password to
+        url: 'https://movilmultimediasa.com/abcMobil/post.php',
+        dataType: 'jsonp',
+        async: false,
+        //json object to sent to the authentication url
+        data: '{"userName": "' + userName + '", "password" : "' + password + '"}',
+        success: function (data) {
+
+        alert(data); 
+        }
+    })
 
 	
 }
