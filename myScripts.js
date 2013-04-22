a44c5fdc4ad9b6de5ffc68108bc4f531aae00902
Servicios de mercadeo <ingenieria@movilmultimediasa.com>
2013-04-22 14:50:13 -0600

    HoyTema semana
    HoyTema semana

--- myScripts.js
+++ myScripts.js
@@ -13,7 +13,21 @@ $.ajax({
 	
 }
 
-function sum(a,b)
+function obtenreTextoSemana()
 {
-	return (a+b);
+alert("ss");
+	return "Estilos de Aprendizaje"+
+"Dummy text of the printing and typesetting industry."+ 
+"has been the industry's standard dummy text ever "+
+"since the 1500s, when an unknown printer took a galley "+
+"of type and scrambled it to make a type specimen book. "+
+"It has survived not only five centuries, but also the leap "+
+"into electronic typesetting, remaining essentially "+
+"unchanged.  It was popularised in the 1960s with the"+ 
+"release of Letraset sheets containing Lorem Ipsum "+
+"passages, and more recently with desktop publishing."+
+"Contrary to popular belief, is not simply random text. "+
+"It has roots in a piece of classical Latin literature from "+
+"45 BC, making it over 2000 years old. Richard McClintock, "+
+";
 }
